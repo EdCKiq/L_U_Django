bbbf711e98eba3b82ae6cc0706348905735c1372
KiqECS <kaiquehelper2019@gmail.com>
2021-01-05 09:36:07 -0300

    Como configurar o uso de forms

--- ms_project/ms_core/views.py
+++ ms_project/ms_core/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
+from .forms import LivrosForm
+from django.contrib import messages
 
 def index(request):
     return render(request, 'index.html')
 
 def livros(request):
-    return render(request, 'livros.html')
+    forms = LivrosForm(request.POST or None)
+    if str(request.method) == "POST":
+        if forms.is_valid():
+            nome = forms.cleaned_data['nome']
+            edit_email = forms.cleaned_data['edit_email']
+            ano_publi = forms.cleaned_data['ano_publi']
+            coment = forms.cleaned_data['coment']
+            
+            messages.success(request,"Enviado com Sucesso!!")
+
+            forms = LivrosForm()
+
+            #print(request.POST) Pode usar para saber seus dados então sendo enviados de forma correta 
+        else:
+            messages.error(request,"Erro no envio")
+            
+    context = {
+        "forms": forms
+    }
+    return render(request, 'livros.html', context)
 
 def compradores(request):
     return render(request, 'compradores.html')
